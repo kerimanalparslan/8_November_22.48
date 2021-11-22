2495bf9d5a1a7fefeb92e1cb67f4858bd66aeed2
Keriman Alparslan <k.alparslan164@gmail.com>
2021-11-23 02:04:45 +0300

    22 kasım ders sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,6 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import { users } from "../data/users.js"
+import Customer from "../modals/customer.js"
 import User from "../modals/user.js"
 import UserService from "../services/userService.js"
 
@@ -14,11 +16,11 @@ let user2 = new User(2,"baran","gökçelikli","muğla");
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
-userService.list()
-userService.getById()
+//userService.list()
+//userService.getById()
 
 
 
@@ -27,3 +29,15 @@ let customer = { id: 1, firstName: "Engin" }
 customer.lastName = "Demiroğ"
 
 console.log(customer.lastName)
+
+console.log("-----------------")
+userService.load()
+
+let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara",25);
+customerToAdd.type = "customer"
+
+userService.add(new Customer(1,"Seda","Yılmaz","Ankara",25))
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomerSorted())
---
+++ src/data/users.js
@@ -0,0 +1,47 @@
+export const users = [
+    {
+        id:1,
+        firstName:"Engin",
+        lastName:"Demiroğ",
+        age:36,
+        creditCardNumber:"123456",
+        city:"Ankara",
+        type: "customer"
+    },
+    {
+        id:2,
+        firstName:"Mustafa",
+        lastName:"Sansar",
+        city:"İstanbul",
+        age:21,
+        creditCardNumber:"654321",
+        type: "customer"
+    },
+    {
+        id:3,
+        firstName:"Cansu",
+        lastName:"Değirmen",
+        city:"İstanbul",
+        salary:8000,
+        age:20,
+        type: "employee"
+    },
+    {
+        id:4,
+        firstName:"Mücahit",
+        lastName:"Kaya",
+        city:"Bolu",
+        age:23,
+        salary:17000,
+        type: "employee"
+    },
+    {
+        id:5,
+        firstName:"Eren",
+        lastName:"Arı",
+        city:"Diayrbakır",
+        age:20,
+        salary:15000,
+        type: "employee"
+    }
+]
